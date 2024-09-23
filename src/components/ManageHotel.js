abd32e6b861c18970feb2947a26288066c705906
leela2706 <saileelavardhan12@gmail.com>
2024-09-23 09:57:45 +0530

    added

--- src/components/ManageHotel.js
+++ src/components/ManageHotel.js
@@ -197,7 +197,7 @@ const ManageHotel = () => {
         <button
           type="submit"
           className={`w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 ${!isFormValid ? 'opacity-50 cursor-not-allowed' : ''}`}
-          disabled={!isFormValid}  
+          disabled={!isFormValid}   
         >
           Submit
         </button>
